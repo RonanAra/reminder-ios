a2c96bba9a02c4fef9d374a5545526f98dbfe5c4
Ronan Fernandes <ronanlm10@hotmail.com>
2026-01-20 08:15:41 -0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Ronan Fernandes on 20/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
