7853459750d717d89d59df9db5d950b7a34ad71e
Ronan Fernandes <ronanlm10@hotmail.com>
2026-02-08 11:20:10 -0300

    feat: Add user defaults manager

---
+++ Reminder/Sources/Features/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Reminder
+//
+//  Created by Ronan Fernandes on 08/02/26.
+//
+
+import Foundation
+
+struct User: Codable {
+    let email: String
+    let isSavedUser: Bool
+}
---
+++ Reminder/Sources/Features/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,30 @@
+//
+//  UserDefaultsManager.swift
+//  Reminder
+//
+//  Created by Ronan Fernandes on 08/02/26.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+    private static let userKey: String = "userKey"
+    
+    static func saveUser(user: User) {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(user) {
+            UserDefaults.standard.set(encoded, forKey: userKey)
+            UserDefaults.standard.synchronize()
+        }
+    }
+    
+    static func getUser() -> User? {
+        if let userData = UserDefaults.standard.data(forKey: userKey) {
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: userData) {
+                return user
+            }
+        }
+        return nil
+    }
+}
